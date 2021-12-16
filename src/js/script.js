81b8b3125060bd7cdb57ecad21320664f4efffc4
Dmitry-Koz <cojevnikov.dmitry2017@gmail.com>
2021-12-16 09:46:00 +0300

    add new block, scroll

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,25 @@ $(window).scroll(function(){
     $('.parallax').css({
         'transform' : 'translate(0%,'+ st/7 +'%)'
     });
+    console.log($('.header').offset().top);
+    console.log($('.pwa').offset().top);
 
+    if ($('.header').offset().top >= $('.pwa').offset().top - 300){
+        $('.header').css({
+            'transform' : 'translateY(0)'
+        })
+    }else{
+        $('.header').css({
+            'transform' : 'translateY(-300px)'
+        })
+    }
+});
+
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 600);
+    return false;
 });
